0d90117f1becf0963bafedb18758046d7e2a21d0
Oleg Savchuk <oleg.savchuk12@gmail.com>
2020-10-09 17:09:25 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 node default {
   file {'/root/README.md':
     ensure  => file,
-    content => "This is Puppet content, after change 2",
+    content => 'This is Puppet content, after change 3',
   }
 }
